67bfa82f520b34beaeb7cb090e4fbcfdb0a9de69
OLGA_D <olgusha0707@ail.ru>
2017-09-06 00:42:12 +0300

    four commit

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: OLGA
- * Date: 05.09.2017
- * Time: 23:46
- */
+$strInFile = file_get_contents('data.txt');
+$dataJSON = json_decode($strInFile);
+?>
+
+<html>
+    <body>
+        <table>
+            <thead>
+            <tr>
+                <td>ИМЯ</td>
+                <td>ФАМИЛИЯ</td>
+                <td>АДРЕС</td>
+                <td>ТЕЛЕФОН</td>
+            </tr>
+            </thead>
+            <?php foreach ($dataJSON as $value) : ?>
+            <tr>
+                <td><?=$value -> firstName;?></td>
+                <td><?=$value -> lastName;?></td>
+                <td><?=$value -> address;?></td>
+                <td><?=$value -> phoneNumber;?></td>
+            </tr>
+            <?php endforeach;?>
+        </table>
+    </body>
+</html>
+
